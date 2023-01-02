9cbba156d81c93cf77fe46c21a610ed6387a8bbd
Greyiron Karina <gkarina@f1r6s9.codam.nl>
2023-01-02 22:32:43 +0100

    first commit

---
+++ Makefile
@@ -0,0 +1,40 @@
+
+NAME=ft_ls
+
+CC=gcc
+
+CFLAGS= -g -Wall -Wextra -Werror
+
+SRCS=		./main.c \
+			./srcs/*.c
+
+OBJS= $(SRCS:%.c=%.o)
+
+HEADERS =	./headers/srcs.h \
+			./headers/utils.h
+
+INCLUDEDIR= ./
+
+all: $(NAME)
+
+$(NAME): $(OBJS) libft.a
+	$(CC) $(CFLAGS) -I $(INCLUDEDIR) $^ -o $@
+
+libft.a:
+	$(MAKE) bonus -C ./libs/libft CC=$(CC)
+	cp ./libs/libft/libft.a .
+
+%.o: %.c $(HEADERS)
+	$(CC) $(CFLAGS) -I $(INCLUDEDIR) -c $< -o $@
+
+clean:
+	$(MAKE) clean -C ./libc/libft
+	rm $(OBJS)
+
+fclean: clean
+	$(MAKE) fclean -C ./libs/libft
+	rm -rf $(NAME) libft.a
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ headers/srcs.h
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   srcs.h                                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: gkarina <gkarina@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/01/02 22:10:11 by gkarina       #+#    #+#                 */
+/*   Updated: 2023/01/02 22:32:08 by gkarina       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LS_H
+# define FT_LS_H
+
+/*
+** write
+** opendir
+** readdir
+** closedir
+** stat
+** lstat
+** getpwuid
+** getgrgid
+** listxattr
+** getxattr
+** time
+** ctime
+** readlink
+** malloc
+** free
+** perror
+** strerror
+** exit
+*/
+
+# include <>
+/*
+*/
+
+
+# include <>
+/*
+*/
+
+
+# include <>
+/*
+*/
+
+
+# include <>
+/*
+*/
+
+
+# include <>
+/*
+*/
+
+
+# include <>
+/*
+*/
+
+
+# include <>
+/*
+*/
+
+
+#endif
