cf1cd58541983fdbd9600a291707e8a56a092c53
Greyiron Karina <gkarina@f0r3s18.codam.nl>
2023-01-31 23:57:42 +0100

    understood about readlink, stat, lstat and etc.

--- main.c
+++ main.c
@@ -54,90 +54,224 @@
 //	}
 // -----------------------
 
+char *get_user(uid_t uid)
+{
+	struct passwd *pwd;
 
- // test dir function
+	if ((pwd = getpwuid(uid)) == NULL)
+		return (NULL);
+	return (pwd->pw_name);
+}
+
+char *get_group(gid_t gid)
+{
+	struct group *grp;
+
+	if ((grp = getgrgid(gid)) == NULL)
+		return (NULL);
+	return (grp->gr_name);
+}
+
+// // test dir function
+//int main(int argc, char **argv)
+//{
+//	t_list *l, *d;
+//	size_t count_elements;
+//	char **elems;
+//
+//	if (argc <= 1)
+//	{
+//		printf("NOT ARGUMENT STUPID\n");
+//		return (0);
+//	}
+//
+//	printf("argument [%s]\n", argv[1]);
+//	l = dir(argv[1]);
+//
+//	count_elements = ft_lstsize(l);
+//
+//	elems = (char**)calloca_to_2d(count_elements);
+//	if (elems == NULL)
+//	{
+//		printf("MALLOC ERROR");
+//		return (-1);
+//	}
+//
+//	char *dirpath = ft_strjoin(argv[1], "/");
+//
+//	size_t i = 0;
+//	while (l != NULL)
+//	{
+//		char *fname = ((struct dirent*)(l->content))->d_name;
+//		if (ft_memcmp(fname, ".", ft_strlen(fname)) == 0
+//		|| ft_memcmp(fname, "..", ft_strlen(fname)) == 0)
+//		{
+//			l = l->next;
+//			continue ;
+//		}
+//		elems[i] = ft_strjoin(dirpath, fname);
+//		if (elems[i] == NULL)
+//		{
+//			free_double_char_array(elems);
+//			printf("MALLOC ERROR");
+//			return (-1);
+//		}
+//		printf("name [%s]\n", fname);
+//		l = l->next;
+//		i++;
+//	}
+//
+//	free(dirpath);
+//
+//	d = clstat(elems);
+//	if (d == NULL)
+//		return (1);
+//	return (0);
+//
+////	// group and user name
+////	while (d != NULL)
+////	{
+////		gid_t gid = ((struct stat*)(d->content))->st_gid;
+////		uid_t uid = ((struct stat*)(d->content))->st_uid;
+////
+////		char *group_name = get_group(gid);
+////		char *user_name = get_user(uid);
+////
+////		printf("group [%s]\n", group_name);
+////		printf("user [%s]\n", user_name);
+////
+////		printf("gid [%u]\n", gid);
+////		d = d->next;
+////	}
+//}
+
+//int main(int argc, char **argv)
+//{
+//	if (argc <= 1)
+//	{
+//		printf("NO ARGUMENT STUPID\n");
+//		return (1);
+//	}
+//
+//	ssize_t buflen, keylen, vallen;
+//	char	*buf, *key, *val;
+//
+//	buflen = listxattr(argv[1], NULL, 0, 0);
+//	if (buflen == -1)
+//	{
+//		perror("listxattr");
+//		return (1);
+//	}
+//	if (buflen == 0)
+//	{
+//		printf("[%s] has no attributes\n", argv[1]);
+//		return (0);
+//	}
+//
+//	buf = ft_calloc(buflen, sizeof(char));
+//	if (buf == NULL)
+//	{
+//		perror("malloc");
+//		return (1);
+//	}
+//
+//	buflen = listxattr(argv[1], buf, buflen, 0);
+//	if (buflen == -1)
+//	{
+//		perror("listxattr");
+//		return (1);
+//	}
+//
+//	key = buf;
+//
+//	while (buflen > 0)
+//	{
+//		printf("attribute key [%s]\n", key);
+//
+//		vallen = getxattr(argv[1], key, NULL, 0, 0, 0);
+//		if (vallen == -1)
+//		{
+//			perror("getxattr");
+//			return (1);
+//		}
+//
+//		if (vallen > 0)
+//		{
+//			val = ft_calloc(vallen + 1, sizeof(char));
+//			if (val == NULL)
+//			{
+//				perror("malloc");
+//				return (1);
+//			}
+//			vallen = getxattr(argv[1], key, val, vallen, 0, 0);
+//			if (vallen == -1)
+//			{
+//				perror("getxattr");
+//			}
+//			else
+//			{
+//				val[vallen] = 0;
+//				printf("attribute value [%s]\n", val);
+//			}
+//			free(val);
+//		}
+//		else if (vallen == 0)
+//		{
+//			printf("<no value>");
+//		}
+//
+//		printf("\n");
+//
+//		keylen = ft_strlen(key) + 1;
+//		buflen -= keylen;
+//		key += keylen;
+//	}
+//	free(buf);
+//	return (0);
+//}
+
+char	*creadlink(char *link)
+{
+	char	*file;
+
+	file = ft_calloc(100, sizeof(char));
+	if (file == NULL)
+	{
+		perror("malloc");
+		return (NULL);
+	}
+	if (readlink(link, file, sizeof(file)) < 0)
+	{
+		perror("readlink");
+		return (NULL);
+	}
+	return (file);
+}
+
 int main(int argc, char **argv)
 {
-	t_list *l, *d;
-	size_t count_elements;
-	char **elems;
-
-	if (argc <= 1)
+	if (argc <= 0)
 	{
 		printf("NOT ARGUMENT STUPID\n");
-		return (0);
-	}
-
-	printf("argument [%s]\n", argv[1]);
-	l = dir(argv[1]);
-
-	count_elements = ft_lstsize(l);
-
-	elems = (char**)calloca_to_2d(count_elements);
-	if (elems == NULL)
-	{
-		printf("MALLOC ERROR");
-		return (-1);
-	}
-
-	char *dirpath = ft_strjoin(argv[1], "/");
-
-	size_t i = 0;
-	while (l != NULL)
-	{
-		char *fname = ((struct dirent*)(l->content))->d_name;
-		if (ft_memcmp(fname, ".", ft_strlen(fname)) == 0
-		|| ft_memcmp(fname, "..", ft_strlen(fname)) == 0)
-		{
-			l = l->next;
-			continue ;
-		}
-		elems[i] = ft_strjoin(dirpath, fname);
-//		printf("ELEMS[i] = [%s]\n", elems[i]);
-//		printf("len elems [%d]\n", len_double_char_array(elems));
-		if (elems[i] == NULL)
-		{
-			free_double_char_array(elems);
-			printf("MALLOC ERROR");
-			return (-1);
-		}
-		printf("name [%s]\n", fname);
-		l = l->next;
-		i++;
-	}
-
-	free(dirpath);
-
-	printf("LEN DFILE [%D]\n", len_double_char_array(elems));
-
-	d = clstat(elems);
-	if (d == NULL)
 		return (1);
-
-	while (d != NULL)
-	{
-		gid_t gid = ((struct stat*)(d->content))->st_gid;
-		uid_t uid = ((struct stat*)(d->content))->st_uid;
-
-		struct group *grp;
-
-		if ((grp = getgrgid(gid)) != NULL)
-		{
-			printf("[%8.8s]\n", grp->gr_name);
-		}
-
-		struct passwd *pwd;
-
-		if ((pwd = getpwuid(uid)) != NULL)
-		{
-			printf("[%-8.8s]\n", pwd->pw_name);
-		}
-
-		printf("gid [%u]\n", gid);
-		d = d->next;
 	}
 
+	char *filename = creadlink(argv[1]);
+	printf("readlink returned [%s] for [%s]\n", filename, argv[1]);
+
+//	if (readlink(argv[1], buf, sizeof(buf)) < 0)
+//	{
+//		perror("readlink");
+//		return (1);
+//	}
+//	else
+//	{
+//		printf("readlink returned [%s] for [%s]\n", buf, argv[1]);
+//	}
 	return (0);
 }
+
+
+
 
 
