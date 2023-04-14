5f497aaae601125a00612e6d8c4d54efada53a09
Greyiron Karina <gkarina@f1r6s2.codam.nl>
2023-04-14 22:05:58 +0200

    new update

--- srcs/sort.c
+++ srcs/sort.c
@@ -16,8 +16,8 @@ int		order_cmp_by_filename(t_fileinfo *first, t_fileinfo *second, int is_inverte
 	res = ft_memcmp(first->filename, second->filename, \
 	max((int)ft_strlen(first->filename), (int)ft_strlen(second->filename)));
 	return (is_inverted == FLAG_INVERTED_NO) ? \
-	is_more(res, 0) : \
-	is_less(res, 0);
+	res > 0 : \
+	res < 0;
 }
 
 int		order_cmp_by_tlastmod(t_fileinfo *first, t_fileinfo *second, int is_inverted)
@@ -60,7 +60,7 @@ void	sort_fileinfo(t_fileinfo **array, size_t count_elems, int (*func)(t_fileinf
 
 void	sort_by_flags(t_fileinfo **files, unsigned char flags)
 {
-	size_t		i, j;
+//	size_t		i, j;
 
 	if (files == NULL)
 		return ;
@@ -70,21 +70,22 @@ void	sort_by_flags(t_fileinfo **files, unsigned char flags)
 	if (is_flag(flags, T_FLAG_SHIFT, T_FLAG_NUM) == 1)
 	{
 		sort_fileinfo(files, len_2array((const void **)files), order_cmp_by_tlastmod, is_flag(flags, R_FLAG_SHIFT, R_FLAG_NUM));
-		size_t	flen = len_2array((const void**)files);
-		i = 0;
-		while (i < flen)
-		{
-			j = i + 1;
-			while (j < flen && files[i]->mtime.tv_sec == files[j]->mtime.tv_sec)
-				j++;
-//			printf("REVERSE FLAG [%d], j = [%zu]\n", is_flag(flags, R_FLAG_SHIFT, R_FLAG_NUM), j);
-//			if (j - i > 1)
-//			{
-//				printf("JJJJJJJ [%zu]\n", j);
-//				printf("FIRST [%s %s %s], SECOND [%s %s %s]\n", files[i]->amonth, files[i]->day_lm, files[i]->time_year_lm, files[j - 1]->amonth, files[j - 1]->day_lm, files[j - 1]->time_year_lm);
-			sort_fileinfo(&files[i], j - i, order_cmp_by_filename, is_flag(flags, R_FLAG_SHIFT, R_FLAG_NUM));
-//			}
-			i += j;
-		}
+//		size_t	flen = len_2array((const void**)files);
+//		i = 0;
+//		while (i < flen)
+//		{
+//			j = i + 1;
+//			while (j < flen && files[i]->mtime.tv_sec == files[j]->mtime.tv_sec)
+//				j++;
+////			printf("i = [%zu] j = [%zu]\n", i, j);
+////			printf("REVERSE FLAG [%d], j = [%zu]\n", is_flag(flags, R_FLAG_SHIFT, R_FLAG_NUM), j);
+////			if (j - i > 1)
+////			{
+////				printf("JJJJJJJ [%zu]\n", j);
+////				printf("FIRST [%s %s %s], SECOND [%s %s %s]\n", files[i]->amonth, files[i]->day_lm, files[i]->time_year_lm, files[j - 1]->amonth, files[j - 1]->day_lm, files[j - 1]->time_year_lm);
+//			sort_fileinfo(&files[i], j - i + 1, order_cmp_by_filename, is_flag(flags, R_FLAG_SHIFT, R_FLAG_NUM));
+////			}
+//			i += j + 1;
+//		}
 	}
 }
