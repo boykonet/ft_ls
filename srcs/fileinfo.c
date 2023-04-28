7898e385688ba41178e98b6358960bab0bfc8409
Greyiron Karina <gkarina@f0r1s14.codam.nl>
2023-04-28 22:23:19 +0200

    update

--- srcs/fileinfo.c
+++ srcs/fileinfo.c
@@ -13,6 +13,20 @@ t_fileinfo	*new_fileinfo(char *path, char *filename)
 	return (f);
 }
 
+/*
+** 1.   directory
+** 2.   symbolic link
+** 3.   socket
+** 4.   pipe
+** 5.   executable
+** 6.   block special
+** 7.   character special
+** 8.   executable with setuid bit set
+** 9.   executable with setgid bit set
+** 10.  directory writable to others, with sticky bit
+** 11.  directory writable to others, without sticky bit
+*/
+
 int	set_color_type(mode_t mode)
 {
 	switch (mode & S_IFMT) {
--- srcs/set/filemode.c
+++ srcs/set/filemode.c
@@ -1,16 +1,96 @@
 #include "../../ls.h"
 
+static void	user_file_mode(mode_t mode, char *p)
+{
+	if (mode & S_IRUSR)
+		*p++ = 'r';
+	else
+		*p++ = '-';
+	if (mode & S_IWUSR)
+		*p++ = 'w';
+	else
+		*p++ = '-';
+	switch (mode & (S_IXUSR | S_ISUID)) {
+		case 0:
+			*p++ = '-';
+			break ;
+		case S_IXUSR:
+			*p++ = 'x';
+			break ;
+		case S_ISUID:
+			*p++ = 'S';
+			break ;
+		case (S_IXUSR | S_ISUID):
+			*p++ = 's';
+			break ;
+	}
+}
+
+static void	group_file_mode(mode_t mode, char *p)
+{
+	if (mode & S_IRGRP)
+		*p++ = 'r';
+	else
+		*p++ = '-';
+	if (mode & S_IWGRP)
+		*p++ = 'w';
+	else
+		*p++ = '-';
+	switch (mode & (S_IXGRP | S_ISGID)) {
+		case 0:
+			*p++ = '-';
+			break ;
+		case S_IXGRP:
+			*p++ = 'x';
+			break ;
+		case S_ISGID:
+			*p++ = 'S';
+			break ;
+		case (S_IXGRP | S_ISGID):
+			*p++ = 's';
+			break ;
+	}
+}
+
+static void	other_file_mode(mode_t mode, char *p)
+{
+	if (mode & S_IROTH)
+		*p++ = 'r';
+	else
+		*p++ = '-';
+	if (mode & S_IWOTH)
+		*p++ = 'w';
+	else
+		*p++ = '-';
+	switch (mode & (S_IXOTH | S_ISVTX)) {
+		case 0:
+			*p++ = '-';
+			break ;
+		case S_IXOTH:
+			*p++ = 'x';
+			break ;
+		case S_ISVTX:
+			*p++ = 'T';
+			break ;
+		case (S_IXOTH | S_ISVTX):
+			*p++ = 't';
+			break ;
+	}
+}
+
 void	cstrmode(mode_t st_mode, char *filemode)
 {
-	const char	permissions[4] = "rwx";
-	size_t		i;
+	char	*p;
 
-	i = 0;
-	while (i < 9)
-	{
-		filemode[i] = (st_mode & (1 << (8 - i))) ? permissions[i % 3] : '-';
-		i++;
-	}
+	if (filemode == NULL)
+		return ;
+	p = filemode;
+	/* user */
+	user_file_mode(st_mode, p);
+	/* group */
+	group_file_mode(st_mode, p + 3);
+	/* other */
+	other_file_mode(st_mode, p + 6);
 }
 
 void	set_filetype(char *filetype, unsigned int type)
@@ -57,7 +137,7 @@ void	set_filemode(char *filepath, mode_t mode, unsigned int filetype, char *file
 {
 	if (filepath == NULL || filemode == NULL)
 		return ;
-	cstrmode(mode, &filemode[1]);
 	set_filetype(&filemode[0], filetype);
+	cstrmode(mode, &filemode[1]);
 	set_file_attribute(filepath, &filemode[10]);
 }
