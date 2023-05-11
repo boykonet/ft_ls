f6cbb0fe18064fc925266ee36049a9be34bf16d2
Greyiron Karina <gkarina@f1r5s14.codam.nl>
2023-05-11 17:41:10 +0200

    update

--- tools/gen_flags/main.c
+++ tools/gen_flags/main.c
@@ -8,46 +8,74 @@ int	main(int argc, char **argv)
 	int	i, j;
 	char	**p;
 
-	i = 0;
-	p = NULL;
+	if (argc == 1)
+		return (0);
+
+
 	argv = argv + 1;
-	while (i < argc - 1)
+	argc = argc - 1;
+
+	i = 0;
+	while (argv[i])
 	{
-		p = calloc(i + 1, sizeof(p));
-		if (!p)
+		if (strlen(argv[i]) != 1)
+		{
+			write(2, "ERROR: INCORRECT INPUT\n", 23);
 			exit(1);
-
-		int	k;
-		k = 0;
-		while (k <= i)
-		{
-			p[k] = argv[0 + k];
-			k++;
 		}
-		k = 0;
-		while (k <= i)
-		{
-			int	m;
-			m = k;
-			while (p[m] != NULL)
-			{
-				printf("[%s]", p[m]);
-				m++;
-			}
-			printf(" ");
-			k++;
-		}
-		printf("\n");
-
-//		j = 0;
-//		while (*(argv + j) != NULL)
-//		{
-//			printf("[%s]\n", *(argv + j));
-//			j++;
-//		}
-		free(p);
 		i++;
 	}
+
+	p = NULL;
+
+	p = calloc(argc + 1, sizeof(char*));
+	if (p == NULL)
+	{
+		write(2, "MALLOC ERROR\n", 13);
+		exit(1);
+	}
+
+	int ind = 0;
+//	while (ind < argc)
+//	{
+		bzero(p, sizeof(char*) * (argc + 1));
+		i = 0;
+
+		int	number = 2;
+
+		int	k = 0;
+		while (k < number)
+		{
+			*(p + k) = argv[k];
+			k++;
+		}
+
+		k = 0;
+		while (p[number - 1] != NULL)
+		{
+			i = 0;
+			while (i < (number - 1))
+			{
+				printf("%s", p[i]);
+				i++;
+			}
+			printf("%s", p[i]);
+			printf("\n");
+			k++;
+			p[i] = argv[i + k];
+		}
+
+//		ind++;
+//	}
+
+	i = 0;
+	while (i < argc + 1)
+	{
+		 p[i] = NULL;
+		 i++;
+	}
+	free(p);
+	p = NULL;
 
 	return (0);
 }
